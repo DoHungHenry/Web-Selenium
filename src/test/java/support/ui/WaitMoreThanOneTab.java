34dbc4b25fd9fd7db3ac7278fc87cedf4d616bf6
DoHungHenry <brisingr2209@gmail.com>
2022-08-02 14:53:55 +0700

    Customize explicit wait with err message

--- src/test/java/support/ui/WaitMoreThanOneTab.java
+++ src/test/java/support/ui/WaitMoreThanOneTab.java
@@ -9,4 +9,9 @@ public class WaitMoreThanOneTab implements ExpectedCondition<Boolean> {
     public Boolean apply(WebDriver driver){
         return driver.getWindowHandles().size() > 1;
     }
+
+    @Override
+    public String toString() {
+        return "Waiting for tab count to be greater than 1";
+    }
 }
